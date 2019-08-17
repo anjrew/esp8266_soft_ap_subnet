c258366a74e40cacbde61874c3c8677d0e2d53e8
earyzhe <earyzhe@gmail.com>
2019-08-17 12:12:39 +0200

    got it to render on the screen

--- esp8266__soft_AP.ino
+++ esp8266__soft_AP.ino
@@ -1,28 +1,65 @@
 
+
+
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+// Variables
+#define SSID "ESPsoftAP_01"
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
+
+// The Webserver
+ESP8266WebServer server(80);           
+
+// PAGES
+#include "Page_Admin.h"
+//#include "global.h"
 
 
 void setup() {
+  
+  EEPROM.begin(512);
   Serial.begin(115200);
   Serial.println("Running setup");
   setupSoftAp();
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.begin("ESP", "password");
-  
+  server.begin();
+  setupRoutes();
+  Serial.println("Setup Finished");
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   delay(1000);
-   Serial.println("Done loop");
+    server.handleClient();
+    Serial.println("Loop");
+    delay(1000);
 }
 
 
 void setupSoftAp()
 {
-  Serial.print("Setting soft-AP ... ");
-  Serial.println(WiFi.softAP("ESPsoftAP_01", "password") ? "Ready" : "Failed!");
-
+  Serial.print("Setting up the soft access point");
+  WiFi.begin("");
+  WiFi.mode(WIFI_AP_STA);
+//  Serial.print("Setting soft-AP configuration ... ");
+//  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+  bool setUpSuccess = WiFi.softAP(SSID, "password");
+  delay(100);
+  Serial.println(setUpSuccess ? "Ready" : "Failed!");
+  delay(1000);
   Serial.print("Soft-AP local network IP address = ");
   Serial.println(WiFi.softAPIP());
 }
+
+void setupRoutes(){
+  Serial.println("Setting up routes");
+  // Admin page
+      server.on ( "/", []() {
+        Serial.println("admin.html");
+        server.send ( 200, "text/html", PAGE_AdminMainPage );  // const char top of page
+      }  );
+  }
